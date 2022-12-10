b320f8f6f7d8c211b21aef9fe6af746f58ec2f05
kaz <kachiiiscool@gmail.com>
2022-12-10 20:31:13 +0900

    cloud-run.tf: roles/run.serviceAgent を追加しました。
    - CloudBuild でビルド時に PERMISSION_DENIED が発生してしまったため追加しました。

--- infra/modules/cloud-run/cloud-run.tf
+++ infra/modules/cloud-run/cloud-run.tf
@@ -17,13 +17,21 @@ resource "google_project_iam_member" "app_runner_member" {
   member  = "serviceAccount:${google_service_account.app_runner.email}"
 }
 
-# Cloud Run 起動者に追加
+# Cloud Run 起動者 に追加
 resource "google_project_iam_member" "cloud_runner_member" {
   project = var.gcp_project_id
   role    = "roles/run.invoker"
   member  = "serviceAccount:${google_service_account.app_runner.email}"
 }
 
+# Cloud Run サービスエージェント に追加
+resource "google_project_iam_member" "cloud_runner_service_agent" {
+  project = var.gcp_project_id
+  role    = "roles/run.serviceAgent"
+  member  = "serviceAccount:${google_service_account.app_runner.email}"
+}
+
+# Secret Manager のシークレット アクセサー に追加
 resource "google_project_iam_member" "secret_accessor_member" {
   project = var.gcp_project_id
   role    = "roles/secretmanager.secretAccessor"
